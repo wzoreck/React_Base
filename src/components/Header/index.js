0c6aae2cb4e7b1d980a9039ae428f1b0b068ea64
Daniel Wzoreck <daniel.wzoreck@sc.senai.br>
2023-02-05 21:18:57 -0300

    Separating actions and reducers

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
-    const botaoClicado = useSelector(state => state.botaoClicado);
+    const botaoClicado = useSelector(state => state.exampleReducer.botaoClicado);
 
     return (
         <Nav>
